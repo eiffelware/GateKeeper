8684c67dca8bc8c1ed524575f9bca7fff3018a1a
Peregrin <46428773+Peregrinn@users.noreply.github.com>
2021-12-14 10:01:40 -0600

    Update v0.2.4

--- src/views/public/js/splash.js
+++ src/views/public/js/splash.js
@@ -2,7 +2,7 @@ const fetch = require('node-fetch');
 var a = document.getElementById('headerText');
 setTimeout(() => { document.getElementById('btnQuit').classList.remove('disabled') }, 1950);
 
-fetch(`https://www.eiffelware.net/api/apps/gatekeeper/0.2.3`, {
+fetch(`https://www.eiffelware.net/api/apps/gatekeeper/0.2.4`, {
     method: 'get'
 }).then((r) => r.json()).then((b) => {
     console.log(b);
